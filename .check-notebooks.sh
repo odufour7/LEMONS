e09f0de1a0e9916fd984e9e8d5e6ad2f660987bb
oscar Dufour <oscardufour@MacBook-Pro-de-oscar.local>
2026-08-11 14:29:57 -0400

    refresh dependencies

--- .check-notebooks.sh
+++ .check-notebooks.sh
@@ -36,5 +36,6 @@ fi
 ## Run nbmake tests on the configuration files notebooks
 uv run pytest --nbmake -n auto ./tutorials/configuration
 
-# Additionally, the user can run nbmake tests on the mechanical-layer notebook on its local machine, provided he set the correct ffmpeg path in the notebook and use the appropriate extension for ../../src/mechanical_layer/build/libCrowdMechanics (.so on Ubuntu, .dylib on macOS).
+# Additionally, the user can run nbmake tests on the mechanical-layer notebook on its local machine, provided he set the correct ffmpeg
+# Comment the following line after you performed the test as ffmpeg is not installed in the Github Actions workflow
 # uv run pytest --nbmake -n auto ./tutorials/mechanical_layer
