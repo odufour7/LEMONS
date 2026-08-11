2af03e3bd71d9ba367a1dde04aff3e8bd6aec7fc
oscar Dufour <oscardufour@MacBook-Pro-de-oscar.local>
2026-08-11 14:25:56 -0400

    refresh dependencies

--- .check-notebooks.sh
+++ .check-notebooks.sh
@@ -37,4 +37,4 @@ fi
 uv run pytest --nbmake -n auto ./tutorials/configuration
 
 # Additionally, the user can run nbmake tests on the mechanical-layer notebook on its local machine, provided he set the correct ffmpeg path in the notebook and use the appropriate extension for ../../src/mechanical_layer/build/libCrowdMechanics (.so on Ubuntu, .dylib on macOS).
-uv run pytest --nbmake -n auto ./tutorials/mechanical_layer
+# uv run pytest --nbmake -n auto ./tutorials/mechanical_layer
